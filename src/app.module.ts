0569a181a458e31ad90fd5fa51361f4fcf9e6dd7
gautam-629 <gautam629@gmail.com>
2024-02-23 19:45:25 +0545

    create vehicle module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
   imports: [AuthModule,
-  MongooseModule.forRoot('mongodb://localhost/nest')],
+  MongooseModule.forRoot('mongodb://localhost/nest'),
+  VehicleModule],
   controllers: [AppController],
   providers: [AppService],
 })
